cdf0da9667ddc0a084648347b488a1f7fd5055ff
Michael Mileusnich <mmileusnich@gmail.com>
2025-05-09 15:25:12 -0500

    fix: add connection manager to agent

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -2,11 +2,12 @@ use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::TcpListener;
 
 use tracing::info;
-use tracing_subscriber::prelude::*;
 
 use core_logic::communications::{Communication, Direction, Message};
 
 use std::io;
+
+const AGNET_PORT: u16 = 8081;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
@@ -17,32 +18,50 @@ async fn main() -> io::Result<()> {
     tracing::subscriber::set_global_default(subscriber)
         .expect("Failed to set global default subscriber");
 
-    // 1. Bind the listener to an address
-    let listener = TcpListener::bind("127.0.0.1:8081").await?;
-    info!("Listening on: {}", listener.local_addr()?);
+    ConnectionManager::try_new()
+        .expect("Failed to create connection manager")
+        .listen()
+        .await
+}
 
-    // 2. Accept incoming connections in a loop
-    loop {
-        // 3. Wait for a new connection
-        let (mut socket, addr) = listener.accept().await?;
-        info!("New client connected: {}", addr);
+pub struct ConnectionManager {
+    listener: TcpListener,
+}
 
-        // 4. Handle the new connection (e.g., read and write data)
-        tokio::spawn(async move {
-            let mut buf = vec![0; 65536]; // 64KB buffer
-            while let Ok(n) = socket.read(&mut buf).await {
-                if n == 0 {
-                    info!("Client disconnected: {}", addr);
-                    break;
+impl ConnectionManager {
+    pub fn try_new() -> io::Result<Self> {
+        let listener = std::net::TcpListener::bind(format!("0.0.0.0:{AGNET_PORT}"))?;
+        let listener = TcpListener::from_std(listener)?;
+
+        Ok(Self { listener })
+    }
+
+    pub async fn listen(&self) -> io::Result<()> {
+        info!("Listening on: {}", self.listener.local_addr()?);
+
+        // 2. Accept incoming connections in a loop
+        loop {
+            // 3. Wait for a new connection
+            let (mut socket, addr) = self.listener.accept().await?;
+            info!("New client connected: {}", addr);
+
+            // 4. Handle the new connection (e.g., read and write data)
+            tokio::spawn(async move {
+                let mut buf = vec![0; 65536]; // 64KB buffer
+                while let Ok(n) = socket.read(&mut buf).await {
+                    if n == 0 {
+                        info!("Client disconnected: {}", addr);
+                        break;
+                    }
+                    // Process the received data
+                    let data = String::from_utf8_lossy(&buf[..n]);
+                    info!("Received from {}: {}", addr, data);
+
+                    // Echo the data back to the client
+                    socket.write_all(data.as_bytes()).await.unwrap();
                 }
-                // Process the received data
-                let data = String::from_utf8_lossy(&buf[..n]);
-                info!("Received from {}: {}", addr, data);
-
-                // Echo the data back to the client
-                socket.write_all(data.as_bytes()).await.unwrap();
-            }
-            info!("Finished processing connection: {}", addr);
-        });
+                info!("Finished processing connection: {}", addr);
+            });
+        }
     }
 }
--- central-command/src/main.rs
+++ central-command/src/main.rs
@@ -28,41 +28,47 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     let (tx, mut rx) = mpsc::channel::<String>(32);
 
-    // Spawn a task to handle incoming connections
-    spawn(async move {
-        while let Ok((stream, addr)) = listener.accept().await {
-            info!("Accepted connection from: {}", addr);
-            let tx_clone = tx.clone();
-            spawn(async move {
-                if let Err(e) = handle_connection(stream, tx_clone).await {
-                    error!("Error handling connection from {}: {}", addr, e);
-                }
-            });
-        }
-    });
+    // // Spawn a task to handle incoming connections
+    // spawn(async move {
+    //     while let Ok((stream, addr)) = listener.accept().await {
+    //         info!("Accepted connection from: {}", addr);
+    //         let tx_clone = tx.clone();
+    //         spawn(async move {
+    //             if let Err(e) = handle_connection(stream, tx_clone).await {
+    //                 error!("Error handling connection from {}: {}", addr, e);
+    //             }
+    //         });
+    //     }
+    // });
 
     // Spawn a task to connect to the server and send data
     spawn(async move {
         let agents = Vec::from(["127.0.0.1:8081"]);
-        for agent in agents.iter() {
+
+        for agent in agents.into_iter() {
             match TcpStream::connect(agent).await {
                 Ok(mut stream) => {
-                    info!("Connected to server!");
-                    while let Some(message) = rx.recv().await {
-                        info!("Sending: {}", message);
-                        if let Err(e) = stream.write_all(message.as_bytes()).await {
-                            error!("Error sending data: {}", e);
-                            break;
-                        }
-                        if let Err(e) = stream.write_all(b"\n").await {
-                            // Add newline as a message delimiter
-                            error!("Error sending newline: {}", e);
-                            break;
-                        }
-                    }
+                    info!("Connected to agent {agent}!");
+
+                    stream
+                        .write_all("Hello from command!".as_bytes())
+                        .await
+                        .unwrap();
+                    // while let Some(message) = rx.recv().await {
+                    //     info!("Sending: {}", message);
+                    //     if let Err(e) = stream.write_all(message.as_bytes()).await {
+                    //         error!("Error sending data: {}", e);
+                    //         break;
+                    //     }
+                    //     if let Err(e) = stream.write_all(b"\n").await {
+                    //         // Add newline as a message delimiter
+                    //         error!("Error sending newline: {}", e);
+                    //         break;
+                    //     }
+                    // }
                 }
                 Err(e) => {
-                    error!("Error connecting to server: {}", e);
+                    error!("Error connecting to agent {agent}: {e}");
                 }
             }
         }
@@ -75,45 +81,45 @@ async fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-async fn handle_connection(
-    mut stream: TcpStream,
-    tx: mpsc::Sender<String>,
-) -> Result<(), Box<dyn Error>> {
-    let addr = stream.peer_addr()?;
-    let mut buffer = [0; 1024];
+// async fn handle_connection(
+//     mut stream: TcpStream,
+//     tx: mpsc::Sender<String>,
+// ) -> Result<(), Box<dyn Error>> {
+//     let addr = stream.peer_addr()?;
+//     let mut buffer = [0; 1024];
 
-    loop {
-        match stream.read(&mut buffer).await {
-            Ok(0) => {
-                info!("Connection with {} closed by peer.", addr);
-                break;
-            }
-            Ok(n) => {
-                match String::from_utf8(buffer[..n].to_vec()) {
-                    Ok(message) => {
-                        info!("Received from {}: {}", addr, message.trim());
-                        // Optionally, broadcast the received message to other connected clients
-                        if tx
-                            .send(format!("{}: {}", addr, message.trim()))
-                            .await
-                            .is_err()
-                        {
-                            error!("Error sending received message to broadcast channel");
-                            break;
-                        }
-                    }
-                    Err(e) => {
-                        error!("Received invalid UTF-8 from {}: {}", addr, e);
-                        break;
-                    }
-                }
-            }
-            Err(e) => {
-                error!("Error reading from {}: {}", addr, e);
-                break;
-            }
-        }
-    }
+//     loop {
+//         match stream.read(&mut buffer).await {
+//             Ok(0) => {
+//                 info!("Connection with {} closed by peer.", addr);
+//                 break;
+//             }
+//             Ok(n) => {
+//                 match String::from_utf8(buffer[..n].to_vec()) {
+//                     Ok(message) => {
+//                         info!("Received from {}: {}", addr, message.trim());
+//                         // Optionally, broadcast the received message to other connected clients
+//                         if tx
+//                             .send(format!("{}: {}", addr, message.trim()))
+//                             .await
+//                             .is_err()
+//                         {
+//                             error!("Error sending received message to broadcast channel");
+//                             break;
+//                         }
+//                     }
+//                     Err(e) => {
+//                         error!("Received invalid UTF-8 from {}: {}", addr, e);
+//                         break;
+//                     }
+//                 }
+//             }
+//             Err(e) => {
+//                 error!("Error reading from {}: {}", addr, e);
+//                 break;
+//             }
+//         }
+//     }
 
-    Ok(())
-}
+//     Ok(())
+// }
